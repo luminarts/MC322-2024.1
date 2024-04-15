c73de0e463bcf2b3be580faa70fda1c35d78a6d9
Gustavo Borba <gustavo.borba2001@gmail.com>
2024-04-14 21:37:44 -0300

    Classe validacao e rearrumação

--- Lab01/Jogador.java
+++ Lab01/Jogador.java
@@ -1,5 +1,4 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 /**
  * Classe Jogador é responsável por armazenar e modificar informações do jogador como nome, cpf, foto e email, além de verificar a validação do cpf e email fornecidos
@@ -67,71 +66,7 @@ public class Jogador {
         this.id = contadorJogador++;
     }
 
-    /**
-     * método que calcula os dígitos verificadores do cpf
-     * @param cpf_reduzido primeiros 9 ou 10 dígitos do cpf
-     * @param peso peso a ser atribuido, para o caso do cpf_reduzido ser 9 ou 10 digitos.
-     * @return retorna o inteiro dígito verificador, isto é, o resto de acordo com as regras previstas para o cálculo dos digitos verificadores
-     */
-    private static int calcularDV(String cpf_reduzido, int peso) {
-        int soma = 0;
-        for (int i = 0; i < cpf_reduzido.length(); i++) {
-            soma += Character.getNumericValue(cpf_reduzido.charAt(i)) * peso--;
-        }
-        int resto = soma % 11;
-        return (resto < 2) ? 0 : (11 - resto);
-    }
-
-    /**
-     * método que valida o cpf com base nos digitos verificadores , na quantidade de digitos e se há diferença entre eles
-     * @param cpf cpf a ser validado
-     * @return retorna true ou false conforme a validação do cpf
-     */
-    public boolean validarCPF(String cpf) {
-        int aux = 0;
-        String cpf_aux = cpf.replaceAll("[^0-9]", "");
-        String cpf_reduzido = cpf_aux.substring(0, cpf_aux.length() - 2);
-
-        if (cpf_aux.length() != 11) {
-            return false;
-        }
-
-        for (int i = 1; i < cpf_aux.length(); i++) {
-            if (cpf_aux.charAt(i) != cpf_aux.charAt(i-1)) {
-                aux = 1;
-            }
-        }
-
-        if (aux == 1) {
-            return false;
-        }
-
-        if (cpf_aux.charAt(9) != calcularDV(cpf_reduzido, 10)) {
-            return false;
-        } else {
-            cpf_reduzido = cpf_aux.substring(0, cpf_aux.length() - 1);
-            if (cpf_aux.charAt(10) != calcularDV(cpf_reduzido, 11)) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-
-        
-    }
     
-    /**
-     * método que valida email conforme seu regex esperado
-     * @param email email a ser analisado
-     * @return retorna verdadeiro se o regex é compatível e falso caso contrário
-     */
-    public boolean validarEmail(String email) {
 
-        String email_regex = "^(.+)@(.+)$";
-
-        Pattern pattern = Pattern.compile(email_regex);
-        Matcher matcher = pattern.matcher(email);
-
-        return matcher.matches();
-    }
+    
 }
--- Lab01/Main.java
+++ Lab01/Main.java
@@ -9,7 +9,15 @@ public class Main {
         Jogador jogador = new Jogador(valor_inicial);
         CartaSorte carta_sorte = new CartaSorte();
         Peca peca = new Peca();
-        
+        Validacao validacao = new Validacao();
+
+        if (!validacao.validarCpf(jogador.getCpf())){
+            System.out.println("CPF Inválido.");
+        }
+
+        if (!validacao.validarEmail(jogador.getEmail())){
+            System.out.println("Email Inválido.");
+        }
         
         
         System.out.println(jogador.getNome());
